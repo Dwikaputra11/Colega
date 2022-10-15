3105c76f99bb82ab5cf518529683e6f9410b63a6
Dwika <dwikariswanda@gmail.com>
2022-10-16 02:18:45 +0700

    Add UI Design in Following Fragment

--- app/src/main/java/com/example/colega/MainActivity.kt
+++ app/src/main/java/com/example/colega/MainActivity.kt
@@ -3,6 +3,16 @@ package com.example.colega
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * TODO:
+ * 1. Sync database with api Work Manager (every 1 hour)
+ * 2. Check application is connected to internet or not
+ * 3. Add Pagination to recycle view
+ * 4. Add Following fragment with article from source that user follow
+ * 5. Implement search from database to search article
+ * 6. Implement drawer to source list page and add profile logout option in there
+ * */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
