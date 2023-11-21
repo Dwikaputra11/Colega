d7bf51e87d079d4568252c394c27dc5e9dce9413
Dwika <dwikariswanda@gmail.com>
2023-11-21 23:19:24 +0700

    Add Cryptography

--- app/src/main/java/com/example/colega/ui/fragment/ChiperFragment.kt
+++ app/src/main/java/com/example/colega/ui/fragment/ChiperFragment.kt
@@ -35,6 +35,63 @@ class ChiperFragment : Fragment() {
         return encrypt(text, 26 - shift) // Decryption is just encryption with a backward shift
     }
 
+    private fun encryptRailFence(text: String, rails: Int): String {
+        val fence = Array(rails) { StringBuilder() }
+        var rail = 0
+        var direction = 1
+
+        for (char in text) {
+            fence[rail].append(char)
+            rail += direction
+
+            if (rail == rails - 1 || rail == 0) {
+                direction *= -1
+            }
+        }
+
+        return fence.joinToString("")
+    }
+
+    fun decryptRailFence(ciphertext: String, rails: Int): String {
+        val fence = Array(rails) { CharArray(ciphertext.length) }
+        var rail = 0
+        var direction = 1
+
+        for (i in ciphertext.indices) {
+            fence[rail][i] = 'X' // Placeholder character
+
+            rail += direction
+
+            if (rail == rails - 1 || rail == 0) {
+                direction *= -1
+            }
+        }
+
+        var index = 0
+        for (i in 0 until rails) {
+            for (j in ciphertext.indices) {
+                if (fence[i][j] == 'X') {
+                    fence[i][j] = ciphertext[index++]
+                }
+            }
+        }
+
+        val result = StringBuilder()
+        rail = 0
+        direction = 1
+
+        for (i in ciphertext.indices) {
+            result.append(fence[rail][i])
+            rail += direction
+
+            if (rail == rails - 1 || rail == 0) {
+                direction *= -1
+            }
+        }
+
+        return result.toString()
+    }
+
 
     interface OnSuccessVerify{
         fun onSuccess()
@@ -58,7 +115,7 @@ class ChiperFragment : Fragment() {
         binding.etKey.setText(shift.toString())
         if(type == 1){
             binding.tvType.text = "Tipe: Encrypte"
-            val msg = encrypt("Dwika Developer 2023!", shift)
+            val msg = encryptRailFence(encrypt("Dwika Developer 2023!", shift), shift)
             binding.etMsg.setText(msg)
             binding.btnDecode.isVisible = false
         }else if(type == 2){
@@ -68,13 +125,17 @@ class ChiperFragment : Fragment() {
         }
 
         binding.btnEncode.setOnClickListener {
-            if(encrypt(binding.etResult.text.toString(), binding.etKey.text.toString().toInt()) == binding.etMsg.text.toString()){
+            val result = binding.etResult.text.toString()
+            val key = binding.etKey.text.toString().toInt()
+            if(encryptRailFence(encrypt(result, key),key) == binding.etMsg.text.toString()){
                 listener.onSuccess()
             }
         }
 
         binding.btnDecode.setOnClickListener {
-            if(decrypt(binding.etResult.text.toString(), binding.etKey.text.toString().toInt()) == binding.etMsg.text.toString()){
+            val result = binding.etResult.text.toString()
+            val key = binding.etKey.text.toString().toInt()
+            if(decrypt(decryptRailFence(result, key), key) == binding.etMsg.text.toString()){
                 listener.onSuccess()
             }
         }
